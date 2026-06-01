e5fb5c9480afdcea808132d75ae1e66e783729e1
Yahyaharoun <yahyaharoun.657@gmail.com>
2026-06-01 19:44:47 +0100

    fix: allow PWA service worker to register again for mobile installation

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './app/App.tsx';
 import './index.css';
 
-// Force clear PWA cache
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations().then((registrations) => {
-    for (const registration of registrations) {
-      registration.unregister();
-    }
-  });
-}
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
